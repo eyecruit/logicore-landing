153b25b2cbe2558bdf487055c7d7b77269d40e56
yashavsarmal30 <businessspaces2@gmail.com>
2025-06-13 01:46:23 +0530

    added extrac page.tsx to debug

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import { Background, Companies, Connect, Container, CTA, Features, Hero, Perks, Pricing, Reviews, Wrapper } from "@/components";
+import { Spotlight } from "@/components/ui/spotlight";
+import { generateMetadata } from "@/functions/metadata";
+
+export const metadata = generateMetadata({
+  title: "LogiCore - Web Development Agency",
+  description: "Expert web development solutions for businesses of all sizes. We create beautiful, functional websites that drive results.",
+});
+
+export default function HomePage() {
+    return (
+        <Background>
+            <Wrapper className="py-20 relative">
+                <Container className="relative">
+                    <Spotlight
+                        className="-top-40 left-0 md:left-60 md:-top-20"
+                        fill="rgba(255, 255, 255, 0.5)"
+                    />
+                    <Hero />
+                </Container>
+                <Container className="py-8 lg:py-20">
+                    <Companies />
+                </Container>
+                <Connect />
+                <Features />
+                <Perks />
+                <Pricing />
+                <Reviews />
+                <CTA />
+            </Wrapper>
+        </Background>
+    )
+} 
