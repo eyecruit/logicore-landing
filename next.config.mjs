5d87b46d4f589beab86d0ef68926b5a3d59c2cc6
eyecruit <eyecruit@gmail.com>
2025-06-13 02:22:21 +0530

    solved errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Configuration for Vercel deployment
+    basePath: '',
     images: {
         remotePatterns: [
             {
