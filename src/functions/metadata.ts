3f3002fcd3d7821cd8d3d905b16862d91f9032b9
eyecruit <eyecruit@gmail.com>
2025-06-15 00:47:23 +0530

    chnages icons

--- src/functions/metadata.ts
+++ src/functions/metadata.ts
@@ -26,13 +26,13 @@ export const generateMetadata = ({
             rel: "icon",
             type: "image/png",
             sizes: "32x32",
-            url: "/icons/favicon-32x32.png"
+            url: "/icons/icon.png"
         },
         {
             rel: "icon",
             type: "image/png",
             sizes: "16x16",
-            url: "/icons/favicon-16x16.png"
+            url: "/icons/icon.png"
         },
     ],
     noIndex = false,
